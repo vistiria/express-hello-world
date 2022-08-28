b59f60402dcfca1d0453704f0bffe4b42f7a9438
Anna <anna.romanowicz@wpengine.com>
2022-08-28 22:28:28 +0200

    kkk

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const merge = require('./code-challenge/merge');
 
 server.get('/', (req, res) => {
-  res.send(`Hello Big Bigg World on port: ${port}`);
+  res.send(`Hello Big Big World on port: ${port}`);
 });
 
 server.get('/envs', (req, res) => {
